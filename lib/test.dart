2056b8d667da08e768293b368ec372f3130570de
Himanshu Yadav <himanshu31072001@gmail.com>
2023-09-09 14:00:49 +0530

    integrated sos page

--- lib/test.dart
+++ lib/test.dart
@@ -5,17 +5,43 @@ class TestWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.red,
-        height: double.infinity,
-        width: double.infinity,
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text("hedjbddgjb",style: TextStyle(color: Colors.black),)
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 235, 88, 78),
+          title: Text('SOS'
+         ),
+        ),
+        body: Center(
+          child: SOSbutton(),
+        ),
+      ),
+    );
+  }
+}
+
+class SOSbutton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return GestureDetector(
+      onTap: ()  {
+        // Handle button tap here
+      
+      },
+      child: Container(
+        width: 300, // Set the width of the button
+        height: 300, // Set the height of the button
+        decoration: BoxDecoration(
+         
+         // Make it a circle
+        ),
+        child: Center(
+          child: Image.asset(
+            'assets/sos.jpg', // Replace with your image asset
+            width: 200, // Set the width of the image
+            height: 200, // Set the height of the image
+          ),
         ),
       ),
     );
